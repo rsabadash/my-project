10711b3d360f000bfa36e43caed2e885915324b8
DESKTOP-5DIJ5EV\Roman <r.v.sabadash@gmail.com>
2019-05-28 18:38:17 +0300

    Add registration.

--- server.js
+++ server.js
@@ -4,15 +4,16 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const connection = require('./utils/database');
-const testRouter = require('./routes/test');
+const authRouter = require('./routes/auth');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'client', 'dist'))); // temporary SRC
 
-app.use(testRouter);
+app.use(authRouter);
 
 app.get('/*', (req, res) => {
 	res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
